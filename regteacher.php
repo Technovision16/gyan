1993e3fd4f804578aeebe28709c88913617dd810
codehimanshu <himanshuagrawal1998@gmail.com>
2016-04-09 02:07:43 +0530

    links to framework

--- regteacher.php
+++ regteacher.php
@@ -3,7 +3,12 @@
 		<title>
 			GYAAN
 		</title>
-
+			<!--Import Google Icon Font-->
+			<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+ 			<!--Import materialize.css-->
+      		<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+			<!--Let browser know website is optimized for mobile-->
+     		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	</head>
 	<body>
 		<header>
@@ -95,6 +100,9 @@
 		</section>
 	<footer>
 	</footer>
-	    <script src="js/init.js"></script>
+		<!--Import jQuery before materialize.js-->
+     	<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      	<script type="text/javascript" src="js/materialize.js"></script>
+      	<script src="js/init.js"></script>
 	</body>
 </html>
